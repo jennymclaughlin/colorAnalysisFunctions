48e9c9bb0c50bed5ab30f6c993f913cdfc23b207
Vivek Joshi <vivekjoshi15@gmail.com>
2018-11-19 10:41:20 +0530

    changes

--- Controllers/S3BucketController.cs
+++ Controllers/S3BucketController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using awsColorAnalysisFunctions.Services;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace awsColorAnalysisFunctions.Controllers
 {   
     [Produces("application/json")]
